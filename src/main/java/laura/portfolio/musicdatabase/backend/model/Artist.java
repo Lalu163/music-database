505d827366e27178a25d8e355ac8be617b1cc1c2
Laura Contreras <m.laura.cp89@gmail.com>
2022-07-22 00:12:58 +0200

    done all the classes

--- src/main/java/laura/portfolio/musicdatabase/backend/model/Artist.java
+++ src/main/java/laura/portfolio/musicdatabase/backend/model/Artist.java
@@ -1,4 +1,77 @@
 package laura.portfolio.musicdatabase.backend.model;
 
+import java.util.Date;
+
 public class Artist {
+
+    private Integer code;
+    private String name;
+    private String surname;
+    private String nationality;
+    private Date birthDate;
+    private Date dateOfDeath;
+
+    public Artist(){
+
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Date getDateOfDeath() {
+        return dateOfDeath;
+    }
+
+    public void setDateOfDeath(Date dateOfDeath) {
+        this.dateOfDeath = dateOfDeath;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "code=" + code +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", birthDate=" + birthDate +
+                ", dateOfDeath=" + dateOfDeath +
+                '}';
+    }
 }
--- src/main/java/laura/portfolio/musicdatabase/backend/model/Genre.java
+++ src/main/java/laura/portfolio/musicdatabase/backend/model/Genre.java
@@ -1,4 +1,45 @@
 package laura.portfolio.musicdatabase.backend.model;
 
 public class Genre {
+
+    private Integer code;
+    private String name;
+    private String observations;
+
+    public Genre(){
+
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getObservations() {
+        return observations;
+    }
+
+    public void setObservations(String observations) {
+        this.observations = observations;
+    }
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "code=" + code +
+                ", name='" + name + '\'' +
+                ", observations='" + observations + '\'' +
+                '}';
+    }
 }
