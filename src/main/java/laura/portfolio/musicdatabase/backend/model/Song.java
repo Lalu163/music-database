43a5de5bc8c22a1fa22a08ad726655fd74fa773b
Laura Contreras <m.laura.cp89@gmail.com>
2022-07-21 21:53:48 +0200

    song class finished

--- src/main/java/laura/portfolio/musicdatabase/backend/model/Song.java
+++ src/main/java/laura/portfolio/musicdatabase/backend/model/Song.java
@@ -14,4 +14,94 @@ public class Song {
     private boolean instrumental;
     private int duration;
 
+    public Song(){
+
+    }
+
+    public String getIsmn() {
+        return ismn;
+    }
+
+    public void setIsmn(String ismn) {
+        this.ismn = ismn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+
+    public Compositor getCompositor() {
+        return compositor;
+    }
+
+    public void setCompositor(Compositor compositor) {
+        this.compositor = compositor;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public boolean isExplicit() {
+        return explicit;
+    }
+
+    public void setExplicit(boolean explicit) {
+        this.explicit = explicit;
+    }
+
+    public boolean isInstrumental() {
+        return instrumental;
+    }
+
+    public void setInstrumental(boolean instrumental) {
+        this.instrumental = instrumental;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "ismn='" + ismn + '\'' +
+                ", title='" + title + '\'' +
+                ", releaseDate=" + releaseDate +
+                ", artist=" + artist +
+                ", compositor=" + compositor +
+                ", genre=" + genre +
+                ", explicit=" + explicit +
+                ", instrumental=" + instrumental +
+                ", duration=" + duration +
+                '}';
+    }
 }
